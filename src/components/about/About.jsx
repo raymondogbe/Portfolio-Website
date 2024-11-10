84ce3a295b0535435d0f9baec7b6bf56a2989530
raymondogbe <debillion13@gmail.com>
2024-11-10 11:01:04 +0200

    new react_ssl_nginx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -8,8 +8,8 @@ import { VscFolderLibrary } from "react-icons/vsc";
 const About = () => {
   return (
     <section id="about">
-      <h5>Get To Know</h5>
-      <h2>About Me</h2>
+      <h5>Get To Know Raymond</h5>
+      <h2>About Raymond</h2>
 
       <div className="container about__container">
         <div className="about__me">
